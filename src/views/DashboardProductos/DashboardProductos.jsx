9d104afa12719d2fc56c2aaa2b52e418e410c70e
Almartinl <almarti.tpv@gmail.com>
2023-05-14 12:25:52 +0200

    add new field in form

--- src/views/DashboardProductos/DashboardProductos.jsx
+++ src/views/DashboardProductos/DashboardProductos.jsx
@@ -11,6 +11,7 @@ import {
   MenuItem,
   OutlinedInput,
   Select,
+  Switch,
   TextField,
   Typography,
 } from "@mui/material";
@@ -25,6 +26,32 @@ export default function DashboardProductos() {
   const [vistaPlano, setVistaPlano] = useState(false);
   const [categoria, setCategoria] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [clima, setClima] = useState(false);
+  const [superposable, setSuperposable] = useState(false);
+  const [sanitario, setSanitario] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [ref, setRef] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [largo, setLargo] = useState("");
+  const [ancho, setAncho] = useState("");
+  const [alto, setAlto] = useState("");
+  const [peso, setPeso] = useState("");
+  const [estructura, setEstructura] = useState("");
+  const [paneles, setPaneles] = useState("");
+  const [cubierta, setCubierta] = useState("");
+  const [suelo, setSuelo] = useState("");
+  const [electricidad, setElectricidad] = useState("");
+  const [puerta70, setPuerta70] = useState("");
+  const [puerta85, setPuerta85] = useState("");
+  const [puerta140, setPuerta140] = useState("");
+  const [ventana100x100, setVentana100x100] = useState("");
+  const [ventana40x40, setVentana40x40] = useState("");
+  const [centana100x80, setVentana100x80] = useState("");
+  const [lavabo, setLavabo] = useState("");
+  const [wc, setWc] = useState("");
+  const [urinario, setUrinario] = useState("");
+  const [duchas, setDuchas] = useState("");
+  const [otros, setOtros] = useState("");
 
   function handlePortada(e) {
     setPortada(e.target.files[0]);
@@ -41,6 +68,16 @@ export default function DashboardProductos() {
   }
   function handleChangeCategoria(e) {
     setCategoria(e.target.value);
+  }
+
+  function handleClima(e) {
+    setClima(e.target.checked);
+  }
+  function handleSuperposable(e) {
+    setSuperposable(e.target.checked);
+  }
+  function handleSanitario(e) {
+    setSanitario(e.target.checked);
   }
 
   const [file, setFile] = useState(null);
@@ -77,12 +114,12 @@ export default function DashboardProductos() {
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
-
   return (
     <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
       <Container maxWidth="md">
         <Box component="form" noValidate /*onSubmit={""}*/ sx={{ mt: 3 }}>
           <Grid container spacing={2}>
+            {/* grid de nombre */}
             <Grid item xs={12} sm={6}>
               <TextField
                 size="small"
@@ -98,6 +135,7 @@ export default function DashboardProductos() {
                 color="success"
               />
             </Grid>
+            {/* grid de referencia */}
             <Grid item xs={12} sm={6}>
               <TextField
                 size="small"
@@ -112,6 +150,7 @@ export default function DashboardProductos() {
                 color="success"
               />
             </Grid>
+            {/* grid de categorias */}
             <Grid item xs={12} md={6}>
               <FormControl fullWidth size="small">
                 <InputLabel id="categoriaDashboard" color="success">
@@ -139,6 +178,7 @@ export default function DashboardProductos() {
                 </Select>
               </FormControl>
             </Grid>
+            {/* grid de descripcion */}
             <Grid item xs={12} sm={6}>
               <TextField
                 size="small"
@@ -351,6 +391,23 @@ export default function DashboardProductos() {
                 color="success"
               />
             </Grid>
+            {/* grid de electricidad */}
+            <Grid item xs={12}>
+              <TextField
+                size="small"
+                multiline
+                rows={2}
+                autoComplete="electricidad"
+                name="electricidad"
+                fullWidth
+                id="electricidad"
+                label="Instalacion electrica"
+                autoFocus
+                //   value={newUsuario.nombre}
+                //   onChange={handleInput}
+                color="success"
+              />
+            </Grid>
             {/* grid de puertas */}
             <Grid item xs={12}>
               <Typography variant="h5" fontWeight="bold" color="#07004e">
@@ -511,7 +568,162 @@ export default function DashboardProductos() {
                 </Grid>
               </Grid>
             </Grid>
+            {/* grid de otras opciones */}
+            <Grid item xs={12}>
+              <Typography variant="h5" fontWeight="bold" color="#07004e">
+                Otras opciones
+              </Typography>
+              <Grid container item xs={12} justifyContent="space-around">
+                <FormControlLabel
+                  value="start"
+                  control={<Switch color="primary" onChange={handleClima} />}
+                  label="Climatizacion"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="start"
+                  control={
+                    <Switch color="primary" onChange={handleSuperposable} />
+                  }
+                  label="Superposable"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="start"
+                  control={
+                    <Switch color="primary" onChange={handleSanitario} />
+                  }
+                  label="Equipo Sanitario"
+                  labelPlacement="start"
+                />
+              </Grid>
+            </Grid>
+            {/* grid de equipamiento sanitario */}
+            {sanitario && (
+              <Grid item xs={12}>
+                <Typography variant="h5" fontWeight="bold" color="#07004e">
+                  Equipamiento Sanitario
+                </Typography>
+                <Grid container item xs={12} flexWrap="nowrap">
+                  <Grid
+                    container
+                    item
+                    alignItems="center"
+                    justifyContent="center"
+                    gap={1}
+                    xs={3}
+                  >
+                    <label for="lavabo">Lavabo: </label>
+                    <Grid item xs={12} sm={4}>
+                      <TextField
+                        size="small"
+                        type="number"
+                        autoComplete="lavabo"
+                        name="lavabo"
+                        id="lavabo"
+                        inputProps={{ min: 0, max: 10, defaultValue: 0 }}
+                        autoFocus
+                        //   value={newUsuario.nombre}
+                        //   onChange={handleInput}
+                        color="success"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    item
+                    alignItems="center"
+                    justifyContent="center"
+                    gap={1}
+                    xs={3}
+                  >
+                    <label for="wc">Wc: </label>
+                    <Grid item xs={12} sm={4}>
+                      <TextField
+                        size="small"
+                        type="number"
+                        autoComplete="wc"
+                        name="wc"
+                        id="wc"
+                        inputProps={{ min: 0, max: 10, defaultValue: 0 }}
+                        autoFocus
+                        //   value={newUsuario.nombre}
+                        //   onChange={handleInput}
+                        color="success"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    item
+                    alignItems="center"
+                    justifyContent="center"
+                    gap={1}
+                    xs={3}
+                  >
+                    <label for="urinario">Urinario: </label>
+                    <Grid item xs={12} sm={4}>
+                      <TextField
+                        size="small"
+                        type="number"
+                        autoComplete="urinario"
+                        name="urinario"
+                        id="urinario"
+                        inputProps={{ min: 0, max: 10, defaultValue: 0 }}
+                        autoFocus
+                        //   value={newUsuario.nombre}
+                        //   onChange={handleInput}
+                        color="success"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    item
+                    alignItems="center"
+                    justifyContent="center"
+                    gap={1}
+                    xs={3}
+                  >
+                    <label for="duchas">Duchas: </label>
+                    <Grid item xs={12} sm={4}>
+                      <TextField
+                        size="small"
+                        type="number"
+                        autoComplete="duchas"
+                        name="duchas"
+                        id="duchas"
+                        inputProps={{ min: 0, max: 10, defaultValue: 0 }}
+                        autoFocus
+                        //   value={newUsuario.nombre}
+                        //   onChange={handleInput}
+                        color="success"
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            )}
+
+            {/* grid de otros componentes */}
+            <Grid item xs={12}>
+              <TextField
+                size="small"
+                multiline
+                rows={2}
+                autoComplete="otros"
+                name="otros"
+                fullWidth
+                id="otros"
+                label="Otros Componentes"
+                autoFocus
+                //   value={newUsuario.nombre}
+                //   onChange={handleInput}
+                color="success"
+              />
+            </Grid>
           </Grid>
+          {/* boton de añadir producto */}
           <Button
             type="submit"
             fullWidth
