b4bc6dd97300496380d4450daeaee46470404865
Almartinl <almarti.tpv@gmail.com>
2023-05-20 11:19:38 +0200

    add languague

---
+++ src/locales/fr/translate.js
@@ -0,0 +1,20 @@
+export const translateFr = {
+  //NavBar
+  buttonIni: "Accueil",
+  buttonProduct: "Produits",
+  buttonConf: "Configurateur",
+  buttonProy: "Nos projets",
+  buttonContact: "Contact",
+  buttonAbout: "Qui sommes-nous",
+
+  //Home
+  tittleCarousel: "Accueillir",
+  subTittleCarousel:
+    "Améliorez votre image de marque grâce à nos installations préfabriquées confortables, fiables et garanties.",
+  tittle2Carousel: "Bungalows sur mesure",
+  subTittle2Carousel:
+    "Nous livrons vos installations préfabriquées dans des délais très courts !",
+  tittle3Carousel: "Structures métalliques",
+  subTittle3Carousel:
+    "Nous avons une boutique en ligne où vous pouvez acheter vos installations.",
+};
